ce4784d6b3639ebceff655fe85a74505e2c64f99
Christian01881 <chrispamo01881@gmail.com>
2023-06-13 21:30:05 +0200

    project finished

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div v-for="project in projects">
+      {{ project.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+  export default {
+    name:'App', 
+    data(){
+      return {
+        projects: [],
+        url: 'http://127.0.0.1:8000/api',
+      }
+    },
+    methods: {
+      pippo(){
+        axios.get(`${this.url}/projects`).then((res)=>{
+          this.projects = res.data.results.data,
+          console.log(this.projects);
+        })
+      }
+    },
+    mounted(){
+      this.pippo();
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
